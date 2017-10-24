6c6a79c5a2e742ca8b9048993997d76d00831cff
Unknown <dig1talscap3@gmail.com>
2017-10-24 00:50:30 -0500

    0.0.2

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,15 +6,17 @@ export default class Landing extends React.Component {
 	renderComponent() {
 		return <div className="box landing">
 			<div className="overlay" />
-			<div className="row main">
-				<div className="col-9 col-offset-1">
-					<h1>Javier Mejia</h1>
-					<div className="line" />
-					<p className="title">Web Developer / Designer</p>
-				</div>
-				<div className="col-2">
-					<Link to="/gallery">Gallery</Link>
-					<span className="chevron-right"></span>
+			<div class="container">
+				<div className="row">
+					<div className="col-9 col-offset-1">
+						<h1>Javier Mejia</h1>
+						<div className="line" />
+						<p className="title">Web Developer / Designer</p>
+					</div>
+					<div className="col-2">
+						<Link to="/gallery">Gallery</Link>
+						<span className="chevron-right"></span>
+					</div>
 				</div>
 			</div>
 		</div>;
