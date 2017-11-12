ef5b6e3a19164212da918e071db247bc0190b0e1
Digit <dig1talscap3@gmail.com>
2017-11-12 00:41:13 -0600

    0.0.6

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,23 +3,38 @@ import { Link } from 'react-router-dom'
 import Layout from './Layout'
 
 export default class Landing extends React.Component {
+	toggleNav() {
+		console.log(123)
+	}
+	
 	renderComponent() {
 		return <div className="box landing">
 			<div className="overlay" />
 			<div className="container">
-				<i className="background">
-					<i className="border">
-						<i className="cross" />
-						<i className="cross" />
-						<i className="overlapped-shape" />
-					</i>
-				</i>
-				<div className="row">
-					<div className="col-9 col-offset-1">
-						<h1>Javier Mejia</h1>
-						<div className="line" />
-						<p className="title">Web Developer <span>&#x0002F;&#x0002F;</span> Designer</p>
+				<div className="background">
+					<div className="i pattern" />
+					<div className="i shape-1" />
+					<div className="i shape-2" />
+					<div className="i shape-3" />
+					<div className="border">
+						<div className="i cross t l" />
+						<div className="i cross t r" />
+						<div className="i cross b l" />
+						<div className="i cross b r" />
+						<div className="i overlapped-shape" />
 					</div>
+					<div className="i shape-4" />
+				</div>
+				<i className="nav-toggle hamburger" onClick={this.toggleNav} />
+				<div className="name">
+					<h1>Javier Mejia</h1>
+					<div className="wrap">
+						<div className="line" />
+					</div>
+					<div className="title">Web Developer</div>
+					<div className="title">Graphic Designer</div>
+				</div>
+				<div className="row">
 					<div className="col-2">
 						<Link to="/gallery">Gallery</Link>
 						<span className="chevron-right"></span>
