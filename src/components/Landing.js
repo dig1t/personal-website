8f5e2bd5af36fb8b9607e09167198ab96212f67c
Digit <dig1talscap3@gmail.com>
2017-11-26 16:33:37 -0600

    0.0.11

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,32 +6,22 @@ import Burger from './UI'
 export default class Landing extends React.Component {
 	renderComponent() {
 		return <section className="landing">
-			<div className="background">
-				<div className="i pattern" />
-				<div className="i shape-1" />
-				<div className="i shape-2" />
-				<div className="i shape-3" />
-				<div className="border">
-					<div className="i cross t l" />
-					<div className="i cross t r" />
-					<div className="i cross b l" />
-					<div className="i cross b r" />
-					<div className="i overlapped-shape" />
-				</div>
-				<div className="i shape-4" />
-			</div>
-			<div className="align-wrap">
-				<div className="name">
+			<div className="background" />
+			<div className="overlay" />
+			<div className="row align-wrap">
+				<div className="col-5 col-offset-1 name">
 					<h1>Javier Mejia</h1>
 					<div className="wrap">
 						<div className="line" />
 					</div>
-					<div className="title">Web Developer</div>
-					<div className="title">Graphic Designer</div>
+					<div className="title one">Web Developer</div>
+					<div className="title two">Graphic Designer</div>
 				</div>
-				<div className="nav-btn">
-					<Link to="/portfolio">Portfolio</Link>
-					<span className="fa fa-chevron-right"></span>
+				<div className="col-5">
+					<div className="nav-btn">
+						<Link to="/portfolio">View my Portfolio</Link>
+						<span className="fa fa-chevron-right"></span>
+					</div>
 				</div>
 			</div>
 		</section>;
