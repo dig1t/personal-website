a09f4565541a2a30b60bfe0ae41c1e5fc8dbc3ce
Javier Mejia <4343867+dig1t@users.noreply.github.com>
2017-12-03 16:27:10 -0600

    0.0.13

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,28 +5,30 @@ import Burger from './UI'
 
 export default class Landing extends React.Component {
 	renderComponent() {
-		return <section className="landing">
-			<div className="background hero" />
-			<div className="background overlay" />
-			<div className="background broken-pattern" />
-			<div className="row align-wrap">
-				<div className="col-5 col-offset-1 name">
-					<h1>
-						<div>Javier</div>
-						<div>Mejia</div>
-					</h1>
-					<div className="title">
-						<div>Web Developer</div>
-						<div>Graphic Designer</div>
+		return <>
+			<section className="landing">
+				<div className="row align-wrap">
+					<div className="col-10 name">
+						<h1>
+							<div>Javier</div>
+							<div>Mejia</div>
+						</h1>
+						<div className="title">
+							<div>Web Developer</div>
+							<div>Graphic Designer</div>
+						</div>
+					</div>
+					<div className="col-2">
+						<Link to="/portfolio" className="nav-btn">View Portfolio</Link>
 					</div>
 				</div>
-				<div className="col-2 col-offset-4">
-					<Link to="/portfolio" className="nav-btn">
-						<span>View Portfolio</span>
-					</Link>
-				</div>
+			</section>
+			<div className="background">
+				<div className="hero" />
+				<div className="overlay" />
+				<div className="broken-pattern" />
 			</div>
-		</section>;
+		</>;
 	}
 	
 	render() {
