99f52f02753c03bd541b702708a549f2fe28cac1
Javier Mejia <4343867+dig1t@users.noreply.github.com>
2017-12-22 19:07:33 -0600

    0.0.16

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,8 +5,13 @@ import Burger from './UI'
 
 export default class Landing extends React.Component {
 	renderComponent() {
-		return <React.Fragment>
+		return [
 			<section className="landing">
+				<div className="background">
+					<div className="hero" />
+					<div className="overlay" />
+					<div className="broken-pattern" />
+				</div>
 				<div className="stripes" />
 				<div className="row align-wrap">
 					<div className="col-6 name">
@@ -19,17 +24,12 @@ export default class Landing extends React.Component {
 							<div>Graphic Designer</div>
 						</div>
 					</div>
-					<div className="col-2 col-offset-4">
-						<Link to="/portfolio" className="nav-btn">View Portfolio</Link>
+					<div className="col-6 nav-btn">
+						<Link to="/portfolio">View Portfolio</Link>
 					</div>
 				</div>
 			</section>
-			<div className="background">
-				<div className="hero" />
-				<div className="overlay" />
-				<div className="broken-pattern" />
-			</div>
-		</React.Fragment>;
+		];
 	}
 	
 	render() {
--- src/components/Store.js
+++ src/components/Store.js
@@ -3,8 +3,8 @@ import Layout from './Layout'
 
 export default class Store extends React.Component {
 	renderComponent() {
-		return <section className="store">
-			<h1>store</h1>
+		return <section className="store align-wrap">
+			<h3>Coming Soon</h3>
 		</section>;
 	}
 	
