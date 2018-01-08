8e262e30196c2e626f525e84004d2e5bed49fab9
Unknown <dig1talscap3@gmail.com>
2018-01-08 01:40:38 -0600

    0.0.29

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { Router } from 'react-router'
 import { createBrowserHistory } from 'history'
 
 const history = createBrowserHistory()
