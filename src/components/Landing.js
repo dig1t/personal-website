f6f149569619163f2fb69a560b62f5842de3bf3c
Javier Mejia <4343867+dig1t@users.noreply.github.com>
2018-02-04 21:29:52 -0600

    0.0.38

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,42 +1,93 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Layout from './Layout'
+import Footer from './Footer'
+
+const aboutMe = `Lorem ipsum dolor sit amet,
+consectetuer adipiscing elit, sed diam nonummy nibh euismod
+tincidunt ut laoreet dolore magna aliquam erat volutpat.
+Ut wisi enim ad minim veniam,`
+
+const featuredProjects = [
+	{
+		img: 'https://picsum.photos/700/500',
+		name: 'featured proj',
+		id: 3643
+	},
+	{
+		img: 'https://picsum.photos/500/500',
+		name: 'project',
+		id: 325643
+	},
+	{
+		img: 'https://picsum.photos/400/500',
+		name: 'featured project',
+		id: 3634643
+	}
+]
 
 export class Landing extends React.Component {
 	renderComponent() {
-		return <section className="landing">
-			<div className="background" />
-			<div className="stripes">
-				<div className="tr" />
-				<div className="bl" />
-			</div>
-			<div className="wrap">
-				<div className="row align-wrap">
-					<div className="col-6 name">
-						<h1>
-							<div>Javier</div>
-							<div>Mejia</div>
-						</h1>
-						<div className="title">
-							<div>Web Developer</div>
-							<div>UI Designer</div>
+		return <React.Fragment>
+			<section className="hero">
+				<div className="background" />
+				<div className="stripes">
+					<div className="tr" />
+					<div className="bl" />
+				</div>
+				<div className="wrap">
+					<div className="row-ng align-wrap">
+						<div className="col-6-ng name">
+							<h1>
+								<div>Javier</div>
+								<div>Mejia</div>
+							</h1>
+							<div className="title">
+								<div>Web Developer</div>
+								<div>UI Designer</div>
+							</div>
+						</div>
+						<div className="col-6-ng nav-btn">
+							<Link to="/portfolio">View Portfolio</Link>
 						</div>
 					</div>
-					<div className="col-6 nav-btn">
-						<Link to="/portfolio">View Portfolio</Link>
+					<div className="page row-ng">
+						<div className="col-12-ng">
+							<span className="number--big">01</span>
+							<span className="text">Welcome</span>
+						</div>
 					</div>
 				</div>
-				<div className="page row">
-					<div className="col-12">
-						<span className="number">01</span>
-						<span className="text">Welcome</span>
-					</div>
+			</section>
+			<section className="row-ng about">
+				<div className="col-10-ng col-offset-1 wrap">
+					<h1 className="heading--serif">About Me</h1>
+					<div className="number">02</div>
+					<p>{aboutMe}</p>
 				</div>
-			</div>
-		</section>
+			</section>
+			<section className="row-ng featured">
+				<div className="col-8-ng col-offset-1">
+					<h1 className="heading heading--serif">Portfolio</h1>
+				</div>
+				<div className="col col-2-ng number heading">03</div>
+				<div className="col-10-ng col-offset-1">
+					<div className="list">{featuredProjects.map(project => (
+						<div key={project.id} className="project">
+							<img src={project.img} />
+							<div className="name">{project.name}</div>
+						</div>
+					))}</div>
+				</div>
+				<div className="col-12-ng">
+					<Link to="/portfolio">View More</Link>
+				</div>
+			</section>
+			<Footer />
+		</React.Fragment>
 	}
 	
 	render() {
-		return <Layout page={this.props.location.pathname}>{this.renderComponent()}</Layout>
+		return <Layout className="landing" page={this.props.location.pathname}>{this.renderComponent()}</Layout>
 	}
 }
