a8606bd1e3958d7574403e268a763643b0cd75ca
Digit <dig1talscap3@gmail.com>
2018-02-07 00:06:01 -0600

    0.0.39

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,12 +11,12 @@ class Portfolio extends React.Component {
 	renderComponent() {
 		return <section className="portfolio">
 			<div className="wrap">
-				<div className="heading row-ng">
+				<div className="row-ng heading align-wrap">
 					<div className="col-6-ng">
 						<h1 className="heading--serif">Portfolio</h1>
 						<p>Here is a list of projects I have worked on.</p>
 					</div>
-					<div className="col-6-ng">
+					<div className="col-6-ng clearfix">
 						<div className="number">02</div>
 					</div>
 				</div>
