82c151bfaed723419bea5bde4e9056684e2727a0
Javier Mejia <4343867+dig1t@users.noreply.github.com>
2018-03-20 22:21:08 -0500

    0.0.42

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -29,22 +29,64 @@ const featuredProjects = [
 export class Landing extends React.Component {
 	renderComponent() {
 		return <React.Fragment>
+			<div className="grid">
+				<div className="col-2" />
+				<div className="col-2" />
+				<div className="col-2" />
+				<div className="col-2" />
+				<div className="col-2" />
+				<div className="col-2" />
+			</div>
 			<section className="hero">
+				<div className="stripe" />
+				<div className="wrap">
+					<div className="row-ng align-wrap">
+						<div className="col-4-ng col-offset-2 name">
+							<div className="stripes">
+								<div className="tr" />
+								<div className="bl" />
+							</div>
+							<div className="wrap">
+								<h1>
+									<div>Javier</div>
+									<div>Mejia</div>
+								</h1>
+								<div className="title">
+									<div>Web Developer</div>
+									<div>UI Designer</div>
+								</div>
+							</div>
+						</div>
+						<div className="col-3-ng col-offset-3 nav-btn">
+							<Link to="/portfolio">View Portfolio</Link>
+						</div>
+					</div>
+					<div className="page row-ng">
+						<div className="col-12-ng">
+							<span className="number--big">01</span>
+							<span className="text">Welcome</span>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section className="hero-old">
 				<div className="background" />
 				<div className="wrap">
-					<div className="stripes">
-						<div className="tr" />
-						<div className="bl" />
-					</div>
 					<div className="row-ng align-wrap">
 						<div className="col-6-ng name">
-							<h1>
-								<div>Javier</div>
-								<div>Mejia</div>
-							</h1>
-							<div className="title">
-								<div>Web Developer</div>
-								<div>UI Designer</div>
+							<div className="stripes">
+								<div className="tr" />
+								<div className="bl" />
+							</div>
+							<div className="wrap">
+								<h1>
+									<div>Javier</div>
+									<div>Mejia</div>
+								</h1>
+								<div className="title">
+									<div>Web Developer</div>
+									<div>UI Designer</div>
+								</div>
 							</div>
 						</div>
 						<div className="col-6-ng nav-btn">
